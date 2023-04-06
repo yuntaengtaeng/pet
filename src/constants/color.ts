d6dd46577bb67e3d33a05c924119e160733c2e1a
yuntaengtaeng <k76253218@gmail.com>
2023-04-06 21:13:29 +0900

    add : color 값 추가

---
+++ src/constants/color.ts
@@ -0,0 +1,30 @@
+export const neutral1 = '#4D4D4D';
+export const neutral2 = '#717171';
+export const neutral3 = '#89939E';
+export const neutral4 = '#ABBED1';
+export const neutral5 = '#F5F7FA';
+
+export const white = '#FFFFFF';
+
+export const primary900 = '#FD790D';
+export const primary800 = '#FE9713';
+export const primary700 = '#FEA717';
+export const primary600 = '#FEBA1B';
+export const primary500 = '#FEC822';
+export const primary400 = '#FED035';
+export const primary300 = '#FEDA57';
+export const primary200 = '#FEE487';
+export const primary100 = '#FEEEB6';
+export const primary50 = '#fef9e2';
+
+export const success = '#1B6E53';
+export const successBg = '#EDF9F0';
+
+export const warning = '#FFA826';
+export const warningBg = '#FFF4EC';
+
+export const info = '#2E5AAC';
+export const infoBg = '#EEF2FA';
+
+export const error = '#E73B2F';
+export const errorBg = '#FFF1F0';
