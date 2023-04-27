e1c3e428a031eb6bf30dab06445d878326072879
yuntaengtaeng <k76253218@gmail.com>
2023-04-27 20:15:27 +0900

    add: kakao, onBoarding 이미지 추가
    - constants/color.ts export 형태에서 enum 형태로 변경

--- src/constants/color.ts
+++ src/constants/color.ts
@@ -1,30 +1,34 @@
-export const neutral1 = '#4D4D4D';
-export const neutral2 = '#717171';
-export const neutral3 = '#89939E';
-export const neutral4 = '#ABBED1';
-export const neutral5 = '#F5F7FA';
+const enum Color {
+  neutral1 = '#4D4D4D',
+  neutral2 = '#717171',
+  neutral3 = '#89939E',
+  neutral4 = '#ABBED1',
+  neutral5 = '#F5F7FA',
 
-export const white = '#FFFFFF';
+  white = '#FFFFFF',
 
-export const primary900 = '#FD790D';
-export const primary800 = '#FE9713';
-export const primary700 = '#FEA717';
-export const primary600 = '#FEBA1B';
-export const primary500 = '#FEC822';
-export const primary400 = '#FED035';
-export const primary300 = '#FEDA57';
-export const primary200 = '#FEE487';
-export const primary100 = '#FEEEB6';
-export const primary50 = '#fef9e2';
+  primary900 = '#FD790D',
+  primary800 = '#FE9713',
+  primary700 = '#FEA717',
+  primary600 = '#FEBA1B',
+  primary500 = '#FEC822',
+  primary400 = '#FED035',
+  primary300 = '#FEDA57',
+  primary200 = '#FEE487',
+  primary100 = '#FEEEB6',
+  primary50 = '#fef9e2',
 
-export const success = '#1B6E53';
-export const successBg = '#EDF9F0';
+  success = '#1B6E53',
+  successBg = '#EDF9F0',
 
-export const warning = '#FFA826';
-export const warningBg = '#FFF4EC';
+  warning = '#FFA826',
+  warningBg = '#FFF4EC',
 
-export const info = '#2E5AAC';
-export const infoBg = '#EEF2FA';
+  info = '#2E5AAC',
+  infoBg = '#EEF2FA',
 
-export const error = '#E73B2F';
-export const errorBg = '#FFF1F0';
+  error = '#E73B2F',
+  errorBg = '#FFF1F0',
+}
+
+export default Color;
