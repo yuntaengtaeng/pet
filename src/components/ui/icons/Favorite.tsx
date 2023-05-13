a4594958416aed99ca64a00e8bfd6abe8910ba18
yuntaengtaeng <k76253218@gmail.com>
2023-05-13 22:21:13 +0900

    add : icon 추가 및 default props 설정
    - add : Alarm, Chat, Favorite, Location, User, Users
    - modify : Plus => props color default color 설정

--- src/components/ui/icons/Favorite.tsx
+++ src/components/ui/icons/Favorite.tsx
@@ -6,11 +6,11 @@ import Color from '../../../constants/color';
 
 interface Props {
   size: number;
-  color?: Color;
   style?: ViewStyle;
+  color?: Color;
 }
 
-const Plus = (props: Props) => {
+const Favorite = (props: Props) => {
   return (
     <Svg
       width={props.size}
@@ -20,16 +20,9 @@ const Plus = (props: Props) => {
       style={props.style}
     >
       <Path
-        d="M12 5V19"
+        d="M12.0063 20.6906L3.51888 13.0028C-1.09384 8.39004 5.68686 -0.466388 12.0063 6.69871C18.3257 -0.466388 25.0757 8.42079 20.4937 13.0028L12.0063 20.6906Z"
         stroke={props.color}
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      />
-      <Path
-        d="M5 12H19"
-        stroke={props.color}
-        stroke-width="1.5"
+        stroke-width="1.55273"
         stroke-linecap="round"
         stroke-linejoin="round"
       />
@@ -37,8 +30,8 @@ const Plus = (props: Props) => {
   );
 };
 
-Plus.defaultProps = {
+Favorite.defaultProps = {
   color: Color.neutral2,
 };
 
-export default Plus;
+export default Favorite;
