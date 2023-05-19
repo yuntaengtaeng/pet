394d2cd0695a6448336d19cbc52652c8d12bf92c
yuntaengtaeng <k76253218@gmail.com>
2023-05-19 22:39:38 +0900

    feat : Check, Camera props 변경

--- src/components/ui/icons/Camera.tsx
+++ src/components/ui/icons/Camera.tsx
@@ -1,37 +1,42 @@
 import * as React from 'react';
 import { ViewStyle } from 'react-native';
 import Svg, { Path, Circle } from 'react-native-svg';
+import Color from '../../../constants/color';
 
 interface Props {
-  width: number;
-  height: number;
+  size: number;
   style?: ViewStyle;
+  color?: Color;
+  circleColor?: Color;
 }
 
-const SvgComponent = (props: Props) => {
-  const { width, height, style } = props;
-
+const Camera = (props: Props) => {
   return (
     <Svg
-      width={width}
-      height={height}
+      width={props.size}
+      height={props.size}
       viewBox="0 0 24 24"
       fill="none"
-      style={style}
+      style={props.style}
     >
-      <Circle cx={12} cy={12} r={12} fill="#FEBA1B" />
+      <Circle cx={12} cy={12} r={12} fill={props.circleColor} />
       <Path
-        stroke="#fff"
+        stroke={props.color}
         d="M5.333 10.971a2.638 2.638 0 0 1 2.638-2.638c.422 0 .803-.251.97-.64l.206-.48A2 2 0 0 1 10.985 6h2.029a2 2 0 0 1 1.839 1.212l.206.482c.167.388.548.64.97.64a2.638 2.638 0 0 1 2.638 2.637v3.362a3.333 3.333 0 0 1-3.334 3.334H8.667a3.333 3.333 0 0 1-3.334-3.334v-3.362Z"
       />
       <Circle
         cx={2}
         cy={2}
         r={2}
-        stroke="#fff"
+        stroke={props.color}
         transform="matrix(-1 0 0 1 14 10.333)"
       />
     </Svg>
   );
 };
-export default SvgComponent;
+Camera.defaultProps = {
+  color: Color.neutral2,
+  circleColor: Color.white,
+};
+
+export default Camera;
--- src/components/ui/icons/Check.tsx
+++ src/components/ui/icons/Check.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { ViewStyle } from 'react-native';
 
 import Svg, { Path } from 'react-native-svg';
+import Color from '../../../constants/color';
 
 interface Props {
   size: number;
   style?: ViewStyle;
+  color?: Color;
 }
 
 const Check = (props: Props) => {
@@ -19,12 +21,16 @@ const Check = (props: Props) => {
     >
       <Path
         d="M9 5L14.3306 11.2191C14.7158 11.6684 14.7158 12.3316 14.3306 12.7809L9 19"
-        stroke="#4D4C4C"
+        stroke={props.color}
         stroke-width="1.5"
         stroke-linecap="round"
       />
     </Svg>
   );
+};
+
+Check.defaultProps = {
+  color: Color.neutral2,
 };
 
 export default Check;
