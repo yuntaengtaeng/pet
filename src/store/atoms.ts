aa21b135e2b38974b7e1febb8e92b3df7a892904
yuntaengtaeng <k76253218@gmail.com>
2023-06-08 04:14:15 +0900

    feat : recoil UserState 추가

--- src/store/atoms.ts
+++ src/store/atoms.ts
@@ -1,6 +1,20 @@
 import { atom } from 'recoil';
+import { User } from '../types/interface';
 
 export const LoadingState = atom({
   key: 'LoadingState',
   default: false,
+});
+
+export const UserState = atom<User>({
+  key: 'UserState',
+  default: {
+    email: '',
+    nickname: '',
+    profileImage: '',
+    petType: '',
+    address: '',
+    accessToken: '',
+    refreshToken: '',
+  },
 });
