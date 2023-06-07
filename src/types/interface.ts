a5636b0c32713ddcdffb151fbbeeab8585e4d01b
yuntaengtaeng <k76253218@gmail.com>
2023-06-08 04:13:52 +0900

    feat : User type 추가
    - email, nickname, profileImage, petType, address, accessToken, refreshToken

--- src/types/interface.ts
+++ src/types/interface.ts
@@ -2,3 +2,13 @@ export interface Location {
   latitude: number;
   longitude: number;
 }
+
+export interface User {
+  email: string;
+  nickname: string;
+  profileImage?: string;
+  petType: string;
+  address: string;
+  accessToken: string;
+  refreshToken: string;
+}
