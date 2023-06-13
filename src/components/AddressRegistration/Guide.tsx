ba6e1a5b69dc695382c0b17623b31cf1c2a88ecd
yuntaengtaeng <k76253218@gmail.com>
2023-06-13 20:31:16 +0900

    feat : AddressRegistration/Guide 컴포넌트 추가
    - 주변 동네를 불러오고 있음 또는 현재 위치를 파악할 수 없는 경우 안내를 보여주기 위한 컴포넌트

---
+++ src/components/AddressRegistration/Guide.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import Color from '../../constants/color';
+import TYPOS from '../ui/typo';
+
+interface Props {
+  topText: string;
+  bottomText: string;
+}
+
+const Guide = ({ topText, bottomText }: Props) => {
+  return (
+    <View style={{ alignItems: 'center', marginTop: 72 }}>
+      <Text style={[TYPOS.body1, { color: Color.neutral2 }]}>{topText}</Text>
+      <Text style={(TYPOS.body1, { color: Color.neutral2 })}>{bottomText}</Text>
+    </View>
+  );
+};
+
+export default Guide;
