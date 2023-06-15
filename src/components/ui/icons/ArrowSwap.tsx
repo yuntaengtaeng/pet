e719b2ae03e40e02771447cc4df53c6470ad260b
yuntaengtaeng <k76253218@gmail.com>
2023-06-15 21:47:32 +0900

    feat : icon/ArrowSwap props color 추가

--- src/components/ui/icons/ArrowSwap.tsx
+++ src/components/ui/icons/ArrowSwap.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { ViewStyle } from 'react-native';
 import Svg, { Path } from 'react-native-svg';
+import Color from '../../../constants/color';
 
 interface Props {
   size: number;
   style?: ViewStyle;
+  color: Color;
 }
 
 const ArrowSwap = (props: Props) => {
@@ -18,12 +20,16 @@ const ArrowSwap = (props: Props) => {
     >
       <Path
         d="M7.99985 19L5.70696 16.7071C5.31643 16.3166 5.31643 15.6834 5.70696 15.2929L7.99985 13M5.99985 16L17.9998 16M16.9999 10L19.2927 7.70711C19.6833 7.31658 19.6833 6.68342 19.2927 6.29289L16.9999 4M18.9999 7L6.99985 7"
-        stroke="#4D4C4C"
+        stroke={props.color}
         stroke-width="1.5"
         stroke-linecap="round"
       />
     </Svg>
   );
+};
+
+ArrowSwap.defaultProps = {
+  color: Color.neutral2,
 };
 
 export default ArrowSwap;
