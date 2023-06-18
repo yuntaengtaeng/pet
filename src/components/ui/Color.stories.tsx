737b2ced2d1089991603c3c23d61b32545ba88df
yuntaengtaeng <k76253218@gmail.com>
2023-06-18 16:45:46 +0900

    docs : Design System/Color storybook 컬러 값 워딩 추가

--- src/components/ui/Color.stories.tsx
+++ src/components/ui/Color.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import Color from '../../constants/color';
+import TYPOS from './typo';
 
 const meta = {
   title: 'Design System/Color',
@@ -13,149 +14,226 @@ export default meta;
 type Story = StoryObj;
 
 export const Black = () => (
-  <View
-    style={{ width: 100, height: 100, backgroundColor: Color.black }}
-  ></View>
+  <View>
+    <View
+      style={{ width: 100, height: 100, backgroundColor: Color.black }}
+    ></View>
+    <Text style={[TYPOS.body1, { marginTop: 8 }]}>{Color.black}</Text>
+  </View>
 );
 
 export const Neutral1 = () => (
-  <View
-    style={{ width: 100, height: 100, backgroundColor: Color.neutral1 }}
-  ></View>
+  <View>
+    <View
+      style={{ width: 100, height: 100, backgroundColor: Color.neutral1 }}
+    ></View>
+    <Text style={[TYPOS.body1, { marginTop: 8 }]}>{Color.neutral1}</Text>
+  </View>
 );
 
 export const Neutral2 = () => (
-  <View
-    style={{ width: 100, height: 100, backgroundColor: Color.neutral2 }}
-  ></View>
+  <View>
+    <View
+      style={{ width: 100, height: 100, backgroundColor: Color.neutral2 }}
+    ></View>
+    <Text style={[TYPOS.body1, { marginTop: 8 }]}>{Color.neutral2}</Text>
+  </View>
 );
 
 export const Neutral3 = () => (
-  <View
-    style={{ width: 100, height: 100, backgroundColor: Color.neutral3 }}
-  ></View>
+  <View>
+    <View
+      style={{ width: 100, height: 100, backgroundColor: Color.neutral3 }}
+    ></View>
+    <Text style={[TYPOS.body1, { marginTop: 8 }]}>{Color.neutral3}</Text>
+  </View>
 );
 
 export const Neutral4 = () => (
-  <View
-    style={{ width: 100, height: 100, backgroundColor: Color.neutral4 }}
-  ></View>
+  <View>
+    <View
+      style={{ width: 100, height: 100, backgroundColor: Color.neutral4 }}
+    ></View>
+    <Text style={[TYPOS.body1, { marginTop: 8 }]}>{Color.neutral4}</Text>
+  </View>
 );
 
 export const Neutral5 = () => (
-  <View
-    style={{ width: 100, height: 100, backgroundColor: Color.neutral5 }}
-  ></View>
+  <View>
+    <View
+      style={{ width: 100, height: 100, backgroundColor: Color.neutral5 }}
+    ></View>
+    <Text style={[TYPOS.body1, { marginTop: 8 }]}>{Color.neutral5}</Text>
+  </View>
 );
 
 export const White = () => (
-  <View
-    style={{ width: 100, height: 100, backgroundColor: Color.white }}
-  ></View>
+  <View>
+    <View
+      style={{ width: 100, height: 100, backgroundColor: Color.white }}
+    ></View>
+    <Text style={[TYPOS.body1, { marginTop: 8 }]}>{Color.white}</Text>
+  </View>
 );
 
 export const Primary900 = () => (
-  <View
-    style={{ width: 100, height: 100, backgroundColor: Color.primary900 }}
-  ></View>
+  <View>
+    <View
+      style={{ width: 100, height: 100, backgroundColor: Color.primary900 }}
+    ></View>
+    <Text style={[TYPOS.body1, { marginTop: 8 }]}>{Color.primary900}</Text>
+  </View>
 );
 
 export const Primary800 = () => (
-  <View
-    style={{ width: 100, height: 100, backgroundColor: Color.primary800 }}
-  ></View>
+  <View>
+    <View
+      style={{ width: 100, height: 100, backgroundColor: Color.primary800 }}
+    ></View>
+    <Text style={[TYPOS.body1, { marginTop: 8 }]}>{Color.primary800}</Text>
+  </View>
 );
 
 export const Primary700 = () => (
-  <View
-    style={{ width: 100, height: 100, backgroundColor: Color.primary700 }}
-  ></View>
+  <View>
+    <View
+      style={{ width: 100, height: 100, backgroundColor: Color.primary700 }}
+    ></View>
+    <Text style={[TYPOS.body1, { marginTop: 8 }]}>{Color.primary700}</Text>
+  </View>
 );
 
 export const Primary600 = () => (
-  <View
-    style={{ width: 100, height: 100, backgroundColor: Color.primary600 }}
-  ></View>
+  <View>
+    <View
+      style={{ width: 100, height: 100, backgroundColor: Color.primary600 }}
+    ></View>
+    <Text style={[TYPOS.body1, { marginTop: 8 }]}>{Color.primary600}</Text>
+  </View>
 );
 
 export const Primary500 = () => (
-  <View
-    style={{ width: 100, height: 100, backgroundColor: Color.primary500 }}
-  ></View>
+  <View>
+    <View
+      style={{ width: 100, height: 100, backgroundColor: Color.primary500 }}
+    ></View>
+    <Text style={[TYPOS.body1, { marginTop: 8 }]}>{Color.primary500}</Text>
+  </View>
 );
 
 export const Primary400 = () => (
-  <View
-    style={{ width: 100, height: 100, backgroundColor: Color.primary400 }}
-  ></View>
+  <View>
+    <View
+      style={{ width: 100, height: 100, backgroundColor: Color.primary400 }}
+    ></View>
+    <Text style={[TYPOS.body1, { marginTop: 8 }]}>{Color.primary400}</Text>
+  </View>
 );
 
 export const Primary300 = () => (
-  <View
-    style={{ width: 100, height: 100, backgroundColor: Color.primary300 }}
-  ></View>
+  <View>
+    <View
+      style={{ width: 100, height: 100, backgroundColor: Color.primary300 }}
+    ></View>
+    <Text style={[TYPOS.body1, { marginTop: 8 }]}>{Color.primary300}</Text>
+  </View>
 );
 
 export const Primary200 = () => (
-  <View
-    style={{ width: 100, height: 100, backgroundColor: Color.primary200 }}
-  ></View>
+  <View>
+    <View
+      style={{ width: 100, height: 100, backgroundColor: Color.primary200 }}
+    ></View>
+    <Text style={[TYPOS.body1, { marginTop: 8 }]}>{Color.primary200}</Text>
+  </View>
 );
 
 export const Primary100 = () => (
-  <View
-    style={{ width: 100, height: 100, backgroundColor: Color.primary100 }}
-  ></View>
+  <View>
+    <View
+      style={{ width: 100, height: 100, backgroundColor: Color.primary100 }}
+    ></View>
+    <Text style={[TYPOS.body1, { marginTop: 8 }]}>{Color.primary100}</Text>
+  </View>
 );
 
 export const Primary50 = () => (
-  <View
-    style={{ width: 100, height: 100, backgroundColor: Color.primary50 }}
-  ></View>
+  <View>
+    <View
+      style={{ width: 100, height: 100, backgroundColor: Color.primary50 }}
+    ></View>
+    <Text style={[TYPOS.body1, { marginTop: 8 }]}>{Color.primary50}</Text>
+  </View>
 );
 
 export const Success = () => (
-  <View
-    style={{ width: 100, height: 100, backgroundColor: Color.success }}
-  ></View>
+  <View>
+    <View
+      style={{ width: 100, height: 100, backgroundColor: Color.success }}
+    ></View>
+    <Text style={[TYPOS.body1, { marginTop: 8 }]}>{Color.success}</Text>
+  </View>
 );
 
 export const SuccessBg = () => (
-  <View
-    style={{ width: 100, height: 100, backgroundColor: Color.successBg }}
-  ></View>
+  <View>
+    <View
+      style={{ width: 100, height: 100, backgroundColor: Color.successBg }}
+    ></View>
+    <Text style={[TYPOS.body1, { marginTop: 8 }]}>{Color.successBg}</Text>
+  </View>
 );
 
 export const Warning = () => (
-  <View
-    style={{ width: 100, height: 100, backgroundColor: Color.warning }}
-  ></View>
+  <View>
+    <View
+      style={{ width: 100, height: 100, backgroundColor: Color.warning }}
+    ></View>
+    <Text style={[TYPOS.body1, { marginTop: 8 }]}>{Color.warning}</Text>
+  </View>
 );
 
 export const WarningBg = () => (
-  <View
-    style={{ width: 100, height: 100, backgroundColor: Color.warningBg }}
-  ></View>
+  <View>
+    <View
+      style={{ width: 100, height: 100, backgroundColor: Color.warningBg }}
+    ></View>
+    <Text style={[TYPOS.body1, { marginTop: 8 }]}>{Color.warningBg}</Text>
+  </View>
 );
 
 export const Info = () => (
-  <View style={{ width: 100, height: 100, backgroundColor: Color.info }}></View>
+  <View>
+    <View
+      style={{ width: 100, height: 100, backgroundColor: Color.info }}
+    ></View>
+    <Text style={[TYPOS.body1, { marginTop: 8 }]}>{Color.info}</Text>
+  </View>
 );
 
 export const InfoBg = () => (
-  <View
-    style={{ width: 100, height: 100, backgroundColor: Color.infoBg }}
-  ></View>
+  <View>
+    <View
+      style={{ width: 100, height: 100, backgroundColor: Color.infoBg }}
+    ></View>
+    <Text style={[TYPOS.body1, { marginTop: 8 }]}>{Color.infoBg}</Text>
+  </View>
 );
 
 export const Error = () => (
-  <View
-    style={{ width: 100, height: 100, backgroundColor: Color.error }}
-  ></View>
+  <View>
+    <View
+      style={{ width: 100, height: 100, backgroundColor: Color.error }}
+    ></View>
+    <Text style={[TYPOS.body1, { marginTop: 8 }]}>{Color.error}</Text>
+  </View>
 );
 
 export const ErrorBg = () => (
-  <View
-    style={{ width: 100, height: 100, backgroundColor: Color.errorBg }}
-  ></View>
+  <View>
+    <View
+      style={{ width: 100, height: 100, backgroundColor: Color.errorBg }}
+    ></View>
+    <Text style={[TYPOS.body1, { marginTop: 8 }]}>{Color.errorBg}</Text>
+  </View>
 );
