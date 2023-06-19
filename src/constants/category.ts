cf18ca84ae572673b880461a6b93ab854b963150
yuntaengtaeng <k76253218@gmail.com>
2023-06-19 20:33:31 +0900

    feat : 강아지 고양이 카테고리 타입 변경

--- src/constants/category.ts
+++ src/constants/category.ts
@@ -3,18 +3,19 @@ import { DogCategory, CatCategory } from '../types/interface';
 export const DOG_CATEGORY: DogCategory[] = [
   '전체',
   '사료&간식',
-  '장난감',
-  '옷',
   '영양제',
-  '목욕&미용',
+  '배변/미용',
+  '장난감',
+  '의류/산책용품',
   '기타',
 ];
+
 export const CAT_CATEGORY: CatCategory[] = [
   '전체',
   '사료&간식',
+  '영양제',
+  '배변/미용',
   '장난감',
-  '캣타워',
-  '캣닢',
-  '배변용품',
+  '캣타워/하우스',
   '기타',
 ];
--- src/types/interface.ts
+++ src/types/interface.ts
@@ -15,7 +15,13 @@ export interface User {
 
 export type PetType = 'dog' | 'cat';
 
-export type DefaultCategory = '전체' | '사료&간식' | '장난감' | '기타';
+export type DefaultCategory =
+  | '전체'
+  | '사료&간식'
+  | '영양제'
+  | '배변/미용'
+  | '장난감'
+  | '기타';
 
-export type DogCategory = DefaultCategory | '옷' | '영양제' | '목욕&미용';
-export type CatCategory = DefaultCategory | '캣타워' | '캣닢' | '배변용품';
+export type DogCategory = DefaultCategory | '의류/산책용품';
+export type CatCategory = DefaultCategory | '캣타워/하우스';
