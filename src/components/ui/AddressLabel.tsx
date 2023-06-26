72804cf860fed2c8e2a321d29d119653860374f1
yuntaengtaeng <k76253218@gmail.com>
2023-06-27 01:42:38 +0900

    feat : ui/AddressLabel 컴포넌트 추가
    - 사용자가 등록 해놓은 주소 목록을 보여줄 때 사용하는 UI

---
+++ src/components/ui/AddressLabel.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { View, Text, Pressable } from 'react-native';
+import SHADOWS from './shadow';
+import TYPOS from './typo';
+import Color from '../../constants/color';
+import Close from './icons/Close';
+
+interface Props {
+  address: string;
+  isSelected?: boolean;
+}
+
+const AddressLabel = ({ address, isSelected }: Props) => {
+  return (
+    <View
+      style={[
+        SHADOWS.shadow4,
+        {
+          padding: 16,
+          borderRadius: 4,
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          backgroundColor: isSelected ? Color.primary50 : Color.white,
+        },
+      ]}
+    >
+      <Text
+        style={[
+          TYPOS.body1,
+          { color: isSelected ? Color.primary700 : Color.black },
+        ]}
+      >
+        {address}
+      </Text>
+      <Pressable>
+        <Close size={24} color={isSelected ? Color.primary700 : Color.black} />
+      </Pressable>
+    </View>
+  );
+};
+
+export default AddressLabel;
