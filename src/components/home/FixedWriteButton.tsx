261cacbf81d4e51e1bb5b6d6495989277a93c784
yuntaengtaeng <k76253218@gmail.com>
2023-06-29 00:44:09 +0900

    feat : Home 글쓰기 버튼 press 시 동네 인증 API 연결 및 로직 처리

--- src/components/home/FixedWriteButton.tsx
+++ src/components/home/FixedWriteButton.tsx
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Pressable, StyleSheet } from 'react-native';
 import Plus from '../ui/icons/Plus';
 import Color from '../../constants/color';
 import { useNavigation } from '@react-navigation/native';
 import { RootStackParamList } from '../../types/navigation';
 import { StackNavigationProp } from '@react-navigation/stack';
+import axios, { AxiosError } from 'axios';
+import { HomeDispatchContext } from './HomeDispatchContext';
 
 const FixedWriteButton = () => {
   const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
 
-  const moveEdit = () => {
+  const moveEdit = async () => {
     navigation.push('EditProduct');
   };
+
+  const dispatch = useContext(HomeDispatchContext);
+
+  const verifyNeighborhood = async () => {
+    try {
+      const { data } = await axios.get('/auth/local-area');
+      moveEdit();
+    } catch (error) {
+      const errorResponse = (error as AxiosError).response;
+      if (errorResponse?.status === 400) {
+        dispatch?.locationVerificationPopup.open();
+      }
+    }
+  };
+
   return (
-    <Pressable style={styles.wrap} onPress={moveEdit}>
+    <Pressable style={styles.wrap} onPress={verifyNeighborhood}>
       <Plus size={32} color={Color.white} />
     </Pressable>
   );
