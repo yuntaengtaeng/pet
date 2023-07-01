783ebe3d33817ce6be132c8574cf905c4759c7b8
yuntaengtaeng <k76253218@gmail.com>
2023-07-01 16:37:09 +0900

    feat : ui/BottomSheet storybook 작성

---
+++ src/components/ui/Bottomsheet.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import BottomSheet from './BottomSheet';
+import { View } from 'react-native';
+
+const meta = {
+  title: 'Design System/BottomSheet',
+  component: BottomSheet,
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<typeof BottomSheet>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const BottomSheetExample: Story = {
+  args: {
+    children: <View></View>,
+    isOpened: false,
+    onClose: () => {},
+    height: 300,
+  },
+};
