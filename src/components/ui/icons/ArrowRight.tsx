cd707be1f1a8d47a250547a71ba795003bb6b188
yuntaengtaeng <k76253218@gmail.com>
2023-07-13 00:20:34 +0900

    feat : ArrowRight Props color 추가

--- src/components/ui/icons/ArrowRight.tsx
+++ src/components/ui/icons/ArrowRight.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import { ViewStyle } from 'react-native';
 import Svg, { Path } from 'react-native-svg';
+import Color from '../../../constants/color';
 
 interface Props {
   size: number;
+  color?: Color;
   style?: ViewStyle;
 }
 
@@ -17,11 +19,15 @@ const ArrowRight = (props: Props) => (
   >
     <Path
       d="M9 5L14.3306 11.2191C14.7158 11.6684 14.7158 12.3316 14.3306 12.7809L9 19"
-      stroke="#4D4C4C"
+      stroke={props.color}
       stroke-width="1.5"
       stroke-linecap="round"
     />
   </Svg>
 );
+
+ArrowRight.defaultProps = {
+  color: Color.neutral2,
+};
 
 export default ArrowRight;
