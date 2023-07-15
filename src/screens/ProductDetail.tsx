239aad257af214b385988b96e4b5dce9dbd3c058
yuntaengtaeng <k76253218@gmail.com>
2023-07-15 19:47:40 +0900

    feat : 제품 상세 상태 변경, 좋아요 API 연동

--- src/screens/ProductDetail.tsx
+++ src/screens/ProductDetail.tsx
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Pressable } from 'react-native';
 import ScrollContainer from '../components/layout/ScrollContainer';
 import AppBar from '../components/ui/AppBar';
 import Profile from '../components/productDetail/Profile';
-// import MyPostFooter from '../components/productDetail/footer/MyPostFooter';
 import OtherPostFooter from '../components/productDetail/footer/OtherPostFooter';
 import Carousel from '../components/ui/carousel/Carousel';
 import ProductInfo from '../components/productDetail/ProductInfo';
@@ -15,6 +14,10 @@ import MyPostFooter from '../components/productDetail/footer/MyPostFooter';
 import Description from '../components/productDetail/Description';
 import ProductStats from '../components/productDetail/ProductStats';
 import Color from '../constants/color';
+import useModal from '../hooks/useModal';
+import BottomSheet from '../components/ui/BottomSheet';
+import TYPOS from '../components/ui/typo';
+import { ProductStatus } from '../types/interface';
 
 export type ProductDetailProps = StackScreenProps<
   RootStackParamList,
@@ -24,6 +27,7 @@ export type ProductDetailProps = StackScreenProps<
 const ProductDetail = ({ navigation, route }: ProductDetailProps) => {
   const { id } = route.params;
   const [data, setData] = useState<ProductDetailType | null>(null);
+  const { isVisible, closeModal, openModal } = useModal();
 
   useEffect(() => {
     const fetch = async () => {
@@ -44,6 +48,42 @@ const ProductDetail = ({ navigation, route }: ProductDetailProps) => {
   if (!data) {
     return null;
   }
+
+  const onChangeStatus = async (status: ProductStatus) => {
+    try {
+      const {
+        data: { salesStatus },
+      } = await axios.patch<{ salesStatus: ProductStatus }>(
+        `/board/used-item/${id}/status`,
+        {
+          salesStatus: status,
+        }
+      );
+
+      const clone = { ...data };
+      setData({ ...clone, salesStatus });
+
+      closeModal();
+    } catch (error) {}
+  };
+
+  const onLikeChangeHandler = async () => {
+    try {
+      const {
+        data: { isLike, likeCount },
+      } = await axios[data.isLike ? 'delete' : 'post']<{
+        isLike: boolean;
+        likeCount: number;
+      }>(`/board/used-item/like/${id}`);
+
+      const clone = { ...data };
+      setData({ ...clone, isLike, likeCount });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const STATUS_MAP: ProductStatus[] = ['판매중', '예약중', '거래완료'];
 
   return (
     <>
@@ -87,7 +127,46 @@ const ProductDetail = ({ navigation, route }: ProductDetailProps) => {
           />
         </View>
       </ScrollContainer>
-      {data.isMe ? <MyPostFooter /> : <OtherPostFooter />}
+      {data.isMe ? (
+        <MyPostFooter onStatusChangeHandler={openModal} />
+      ) : (
+        <OtherPostFooter
+          onLikeChangeHandler={onLikeChangeHandler}
+          isLike={data.isLike}
+        />
+      )}
+      <BottomSheet
+        isOpened={isVisible}
+        onClose={() => {
+          closeModal();
+        }}
+        height={280}
+      >
+        <View
+          style={{
+            paddingHorizontal: 24,
+            width: '100%',
+            height: '100%',
+          }}
+        >
+          <View style={{ paddingVertical: 16 }}>
+            <Text style={[TYPOS.headline3, { color: Color.black }]}>
+              상태변경
+            </Text>
+          </View>
+          {STATUS_MAP.map((text) => (
+            <Pressable
+              style={{ paddingVertical: 16 }}
+              key={text}
+              onPress={() => {
+                onChangeStatus(text);
+              }}
+            >
+              <Text style={[TYPOS.body1, { color: Color.black }]}>{text}</Text>
+            </Pressable>
+          ))}
+        </View>
+      </BottomSheet>
     </>
   );
 };
