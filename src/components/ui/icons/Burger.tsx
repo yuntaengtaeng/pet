3a188d099fdda546644c6fd1aad13333e942824d
yuntaengtaeng <k76253218@gmail.com>
2023-07-18 05:47:42 +0900

    feat : Burger Icon Props Color Add

--- src/components/ui/icons/Burger.tsx
+++ src/components/ui/icons/Burger.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { ViewStyle } from 'react-native';
 
 import Svg, { Path } from 'react-native-svg';
+import Color from '../../../constants/color';
 
 interface Props {
   size: number;
+  color?: Color;
   style?: ViewStyle;
 }
 
@@ -19,24 +21,28 @@ const Burger = (props: Props) => {
     >
       <Path
         d="M20 6L4 6"
-        stroke="#4D4C4C"
+        stroke={props.color}
         stroke-width="1.5"
         stroke-linecap="round"
       />
       <Path
         d="M20 12L4 12"
-        stroke="#4D4C4C"
+        stroke={props.color}
         stroke-width="1.5"
         stroke-linecap="round"
       />
       <Path
         d="M20 18H4"
-        stroke="#4D4C4C"
+        stroke={props.color}
         stroke-width="1.5"
         stroke-linecap="round"
       />
     </Svg>
   );
+};
+
+Burger.defaultProps = {
+  color: Color.neutral2,
 };
 
 export default Burger;
