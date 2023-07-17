61ea9c2162fac43cc8b3e364dbf30de2456c4d8c
yuntaengtaeng <k76253218@gmail.com>
2023-07-18 05:48:33 +0900

    fix : 제품 목록 제품이 없는 경우 EmptyList 컴포넌트에서 상품 등록하기 Button Press 시 상품 글 작성 이동 기능 추가

--- src/components/home/EmptyList.tsx
+++ src/components/home/EmptyList.tsx
@@ -3,8 +3,17 @@ import { View, Text } from 'react-native';
 import Color from '../../constants/color';
 import TYPOS from '../ui/typo';
 import Button from '../ui/buttons/Button';
+import { useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { RootStackParamList } from '../../types/navigation';
 
 const EmptyList = () => {
+  const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
+
+  const moveEdit = async () => {
+    navigation.push('EditProduct');
+  };
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', paddingHorizontal: 16 }}>
       <View style={{ alignItems: 'center', flex: 1, justifyContent: 'center' }}>
@@ -16,7 +25,12 @@ const EmptyList = () => {
         </Text>
       </View>
       <View style={{ marginBottom: 24 }}>
-        <Button label="상품 등록하기" />
+        <Button
+          label="상품 등록하기"
+          onPressHandler={() => {
+            moveEdit();
+          }}
+        />
       </View>
     </View>
   );
