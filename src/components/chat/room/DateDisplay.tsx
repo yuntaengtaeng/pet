2cc269fa03ce110ae1bfc01be1830d780bbfaefb
yuntaengtaeng <k76253218@gmail.com>
2023-08-11 03:18:40 +0900

    feat : 채팅방 날짜 표시 컴포넌트 추가

---
+++ src/components/chat/room/DateDisplay.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import TYPOS from '../../ui/typo';
+import Color from '../../../constants/color';
+
+const DateDisplay = () => {
+  return (
+    <View style={{ alignItems: 'center' }}>
+      <Text style={[TYPOS.body3, { color: Color.neutral3 }]}>
+        2023년 2월 18일
+      </Text>
+    </View>
+  );
+};
+
+export default DateDisplay;
