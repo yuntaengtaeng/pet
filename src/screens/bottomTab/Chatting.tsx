0738aba163a791710ec693106bd55b004374b2fa
yuntaengtaeng <k76253218@gmail.com>
2023-08-12 00:36:02 +0900

    feat : dummy_data를 활용하여 Chatting Tab 화면 구현

--- src/screens/bottomTab/Chatting.tsx
+++ src/screens/bottomTab/Chatting.tsx
@@ -4,27 +4,142 @@ import {
   RootStackParamList,
   TabNavigatorParamList,
 } from '../../types/navigation';
-import Container from '../../components/layout/Container';
-import { Text } from 'react-native';
+import { FlatList, Text, View } from 'react-native';
 import TYPOS from '../../components/ui/typo';
 import {
   CompositeNavigationProp,
   useNavigation,
 } from '@react-navigation/native';
 import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
+import ChatRoomItem from '../../components/ui/ChatRoomItem';
+import Color from '../../constants/color';
+import EmptyList from '../../components/chat/EmptyList';
 
 type HomeScreenProps = CompositeNavigationProp<
   BottomTabNavigationProp<TabNavigatorParamList, 'Chatting'>,
   StackNavigationProp<RootStackParamList>
 >;
 
-const Chatting = () => {
-  const navigation = useNavigation<HomeScreenProps>();
+const dummy_data = [
+  {
+    roomId: '1',
+    roomName: '냥냥펀치',
+    image:
+      'https://petmily-images.s3.amazonaws.com/profileImages/dbsxo360@naver.com/dbsxo360@naver.com.jpeg',
+    region: '신림동',
+    timeStamp: '2주',
+    content: '1주일 뒤에 공구 진행하겠습니다~~',
+    isNotificationEnabled: true,
+    isPinned: true,
+  },
+  {
+    roomId: '2',
+    roomName: '냥냥펀치',
+    image:
+      'https://petmily-images.s3.amazonaws.com/profileImages/dbsxo360@naver.com/dbsxo360@naver.com.jpeg',
+    region: '신림동',
+    timeStamp: '2주',
+    content: '1주일 뒤에 공구 진행하겠습니다~~',
+  },
+  {
+    roomId: '3',
+    roomName: '냥냥펀치',
+    image:
+      'https://petmily-images.s3.amazonaws.com/profileImages/dbsxo360@naver.com/dbsxo360@naver.com.jpeg',
+    region: '신림동',
+    timeStamp: '2주',
+    content: '1주일 뒤에 공구 진행하겠습니다~~',
+    isNotificationEnabled: true,
+  },
+  {
+    roomId: '4',
+    roomName: '냥냥펀치',
+    image:
+      'https://petmily-images.s3.amazonaws.com/profileImages/dbsxo360@naver.com/dbsxo360@naver.com.jpeg',
+    region: '신림동',
+    timeStamp: '2주',
+    content: '1주일 뒤에 공구 진행하겠습니다~~',
+    isNotificationEnabled: true,
+  },
+  {
+    roomId: '5',
+    roomName: '냥냥펀치',
+    image:
+      'https://petmily-images.s3.amazonaws.com/profileImages/dbsxo360@naver.com/dbsxo360@naver.com.jpeg',
+    region: '신림동',
+    timeStamp: '2주',
+    content: '1주일 뒤에 공구 진행하겠습니다~~',
+    isNotificationEnabled: true,
+  },
+  {
+    roomId: '6',
+    roomName: '냥냥펀치',
+    image:
+      'https://petmily-images.s3.amazonaws.com/profileImages/dbsxo360@naver.com/dbsxo360@naver.com.jpeg',
+    region: '신림동',
+    timeStamp: '2주',
+    content: '1주일 뒤에 공구 진행하겠습니다~~',
+    isNotificationEnabled: true,
+  },
+  {
+    roomId: '7',
+    roomName: '냥냥펀치',
+    image:
+      'https://petmily-images.s3.amazonaws.com/profileImages/dbsxo360@naver.com/dbsxo360@naver.com.jpeg',
+    region: '신림동',
+    timeStamp: '2주',
+    content: '1주일 뒤에 공구 진행하겠습니다~~',
+  },
+  {
+    roomId: '8',
+    roomName: '냥냥펀치',
+    image:
+      'https://petmily-images.s3.amazonaws.com/profileImages/dbsxo360@naver.com/dbsxo360@naver.com.jpeg',
+    region: '신림동',
+    timeStamp: '2주',
+    content: '1주일 뒤에 공구 진행하겠습니다~~',
+  },
+  {
+    roomId: '9',
+    roomName: '냥냥펀치',
+    image:
+      'https://petmily-images.s3.amazonaws.com/profileImages/dbsxo360@naver.com/dbsxo360@naver.com.jpeg',
+    region: '신림동',
+    timeStamp: '2주',
+    content: '1주일 뒤에 공구 진행하겠습니다~~',
+  },
+  {
+    roomId: '10',
+    roomName: '냥냥펀치',
+    image:
+      'https://petmily-images.s3.amazonaws.com/profileImages/dbsxo360@naver.com/dbsxo360@naver.com.jpeg',
+    region: '신림동',
+    timeStamp: '2주',
+    content: '1주일 뒤에 공구 진행하겠습니다~~',
+  },
+];
 
+const Chatting = () => {
   return (
-    <Container>
-      <Text style={[TYPOS.headline1]}>Chatting</Text>
-    </Container>
+    <>
+      <View
+        style={{
+          paddingHorizontal: 24,
+          paddingVertical: 16,
+          backgroundColor: Color.white,
+        }}
+      >
+        <Text style={[TYPOS.headline3, { color: Color.black }]}>채팅</Text>
+      </View>
+      <FlatList
+        contentContainerStyle={{ ...(!dummy_data.length && { flex: 1 }) }}
+        keyExtractor={(item) => item.roomId}
+        data={dummy_data}
+        showsVerticalScrollIndicator={false}
+        renderItem={(data) => <ChatRoomItem {...data.item} />}
+        ListEmptyComponent={<EmptyList />}
+      />
+    </>
   );
 };
 
