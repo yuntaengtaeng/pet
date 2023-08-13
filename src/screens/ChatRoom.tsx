6572ba63fe1c5292d5da8abbd7d73a3315bcfc73
yuntaengtaeng <k76253218@gmail.com>
2023-08-13 20:56:51 +0900

    feat : Screen ChatRoom UI 구현

--- src/screens/ChatRoom.tsx
+++ src/screens/ChatRoom.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { StackScreenProps } from '@react-navigation/stack';
 import { RootStackParamList } from '../types/navigation';
+import TYPOS from '../components/ui/typo';
 import AppBar from '../components/ui/AppBar';
-import { ScrollView } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
+import Color from '../constants/color';
+import Burger24 from '../components/ui/icons/Burger24';
+import DateDisplay from '../components/chat/room/DateDisplay';
+import ChatBubble from '../components/ui/ChatBubble';
+import Input from '../components/chat/room/Input';
 
 export type OnboardingScreenProps = StackScreenProps<
   RootStackParamList,
@@ -12,10 +18,49 @@ export type OnboardingScreenProps = StackScreenProps<
 const ChatRoom = ({ navigation, route }: OnboardingScreenProps) => {
   return (
     <>
-      <AppBar />
+      <AppBar
+        leftContent={
+          <View>
+            <Text style={[TYPOS.headline3, { color: Color.black }]}>
+              초코코
+            </Text>
+            <Text style={[TYPOS.body3, { color: Color.neutral1 }]}>역삼동</Text>
+          </View>
+        }
+        rightContent={<Burger24 color={Color.black} />}
+      />
       <ScrollView
-        contentContainerStyle={{ paddingHorizontal: 16 }}
-      ></ScrollView>
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          backgroundColor: Color.white,
+          flex: 1,
+        }}
+      >
+        <DateDisplay />
+        <ChatBubble
+          message="안녕하세여"
+          isSentByMe={true}
+          timeStamp="오후 9:43"
+        />
+        <ChatBubble message="구매하고 싶은데" isSentByMe={true} />
+        <ChatBubble
+          message="택배거래도 하실까요?"
+          isSentByMe={true}
+          timeStamp="오후 9:44"
+        />
+        <ChatBubble message="안녕하세요~!" isSentByMe={false} />
+        <ChatBubble
+          message="가급적 직거래를 선호해서요. 혹시 어디쪽에 사시나요?"
+          isSentByMe={false}
+          timeStamp="오후 9:44"
+        />
+        <ChatBubble
+          message="시간이 안되시면 택배거래도 가능합니다."
+          isSentByMe={false}
+          timeStamp="오후 9:45"
+        />
+      </ScrollView>
+      <Input />
     </>
   );
 };
