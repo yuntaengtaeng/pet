9b2d7d3aca8502375c6f25560a4667a66c9bf077
yuntaengtaeng <k76253218@gmail.com>
2023-08-29 14:26:07 +0900

    add : WheelPicker storybook

---
+++ src/components/ui/WheelPicker.stories.ts
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import WheelPicker from './WheelPicker';
+
+const meta = {
+  title: 'UI/WheelPicker',
+  component: WheelPicker,
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<typeof WheelPicker>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const WheelPickerExample: Story = {
+  args: {
+    items: ['apple', 'banana', 'orange'],
+    onIndexChange: () => {},
+    itemHeight: 44,
+  },
+};
