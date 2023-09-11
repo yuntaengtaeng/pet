d2c48f3f649c3a3089c893f5ca8a484916ba0dfa
yuntaengtaeng <k76253218@gmail.com>
2023-09-11 21:40:21 +0900

    add : design system TextIconButton component

---
+++ src/components/ui/buttons/TextIconButton.tsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import { Pressable, Text, StyleSheet, ViewStyle } from 'react-native';
+import Color from '../../../constants/color';
+import TYPOS from '../typo';
+import Right16 from '../icons/Right16';
+
+type Type = 'primary' | 'secondary';
+
+export interface Props {
+  label: string;
+  buttonType?: Type;
+  width?: number;
+  disabled?: boolean;
+  onPressHandler?: () => void;
+  style?: ViewStyle;
+}
+
+const TextIconButton = (props: Props) => {
+  const {
+    label,
+    width,
+    buttonType = 'primary',
+    disabled,
+    onPressHandler,
+    style,
+  } = props;
+
+  const color = (() => {
+    if (disabled) {
+      return Color.neutral3;
+    }
+
+    if (buttonType === 'primary') {
+      return Color.primary900;
+    } else if (buttonType === 'secondary') {
+      return Color.neutral2;
+    }
+  })();
+
+  const backgroundColor = (pressed: boolean) => {
+    if (disabled) {
+      if (buttonType === 'primary') {
+        return Color.neutral4;
+      } else {
+        return Color.white;
+      }
+    } else if (pressed) {
+      return Color.primary50;
+    } else {
+      return Color.white;
+    }
+  };
+
+  return (
+    <Pressable
+      onPress={() => {
+        if (onPressHandler) {
+          onPressHandler();
+        }
+      }}
+      style={({ pressed }) => [
+        {
+          backgroundColor: backgroundColor(pressed),
+          height: 32,
+          alignItems: 'center',
+          justifyContent: 'center',
+          borderRadius: 5,
+          paddingHorizontal: 8,
+          flexDirection: 'row',
+        },
+        {
+          width,
+        },
+      ]}
+    >
+      <Text
+        style={[
+          TYPOS.small,
+          {
+            color,
+          },
+        ]}
+      >
+        {label}
+      </Text>
+      <Right16 color={color as Color} />
+    </Pressable>
+  );
+};
+
+export default TextIconButton;
