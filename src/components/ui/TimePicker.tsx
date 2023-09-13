aefa2c367add086507aeaf92ae5a7ef8f7d52555
yuntaengtaeng <k76253218@gmail.com>
2023-09-13 22:11:13 +0900

    refactor : TimePicker handleIndexChange parameter Type number => string 변경 및 ref에 값 자체를 할당하도록 변경

--- src/components/ui/TimePicker.tsx
+++ src/components/ui/TimePicker.tsx
@@ -23,16 +23,16 @@ const TimePicker = ({ onTimeChange, itemHeight }: Props) => {
   const selectedHour = useRef('');
   const selectedMinute = useRef('');
 
-  const handleIndexChange = (category: string, index: number) => {
+  const handleIndexChange = (category: string, item: string) => {
     switch (category) {
       case 'ampm':
-        selectedAMPM.current = ampmItems[index];
+        selectedAMPM.current = item;
         break;
       case 'hour':
-        selectedHour.current = hourItems[index];
+        selectedHour.current = item;
         break;
       case 'minute':
-        selectedMinute.current = minuteItems[index];
+        selectedMinute.current = item;
         break;
       default:
         throw new Error('Invalid time category');
@@ -54,17 +54,17 @@ const TimePicker = ({ onTimeChange, itemHeight }: Props) => {
     >
       <WheelPicker
         items={ampmItems}
-        onIndexChange={(index) => handleIndexChange('ampm', index)}
+        onItemChange={(item) => handleIndexChange('ampm', item)}
         itemHeight={itemHeight}
       />
       <WheelPicker
         items={hourItems}
-        onIndexChange={(index) => handleIndexChange('hour', index)}
+        onItemChange={(item) => handleIndexChange('hour', item)}
         itemHeight={itemHeight}
       />
       <WheelPicker
         items={minuteItems}
-        onIndexChange={(index) => handleIndexChange('minute', index)}
+        onItemChange={(item) => handleIndexChange('minute', item)}
         itemHeight={itemHeight}
       />
       <View
