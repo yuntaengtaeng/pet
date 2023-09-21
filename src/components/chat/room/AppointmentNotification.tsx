a5fe440b91bbeeadf9299e2c23b7f68fb1a24bbf
yuntaengtaeng <k76253218@gmail.com>
2023-09-21 21:06:13 +0900

    feat : AppointmentNotification Card Header에 수정하기 버튼 추가

--- src/components/chat/room/AppointmentNotification.tsx
+++ src/components/chat/room/AppointmentNotification.tsx
@@ -1,30 +1,59 @@
-import { View, Text } from 'react-native';
+import { View, Text, Pressable } from 'react-native';
 import Color from '../../../constants/color';
 import TYPOS from '../../ui/typo';
+import Right16 from '../../ui/icons/Right16';
 
 interface Props {
   timestamp: string;
   content: string;
+  onModifyHandler: () => void;
 }
 
-const AppointmentNotification = ({ timestamp, content }: Props) => {
+const AppointmentNotification = ({
+  timestamp,
+  content,
+  onModifyHandler,
+}: Props) => {
   return (
     <View
       style={{ borderWidth: 1, borderColor: Color.neutral4, borderRadius: 8 }}
     >
-      <View style={{ backgroundColor: Color.primary50 }}>
+      <View
+        style={{
+          backgroundColor: Color.primary50,
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          paddingHorizontal: 16,
+          paddingVertical: 12,
+        }}
+      >
         <Text
           style={[
             TYPOS.headline4,
             {
               color: Color.primary900,
-              paddingHorizontal: 16,
-              paddingVertical: 12,
             },
           ]}
         >
           {timestamp}
         </Text>
+        <Pressable
+          style={{ flexDirection: 'row', alignItems: 'center' }}
+          onPress={onModifyHandler}
+        >
+          <Text
+            style={[
+              TYPOS.small,
+              {
+                color: Color.neutral2,
+              },
+            ]}
+          >
+            수정하기
+          </Text>
+          <Right16 color={Color.neutral2} />
+        </Pressable>
       </View>
       <View style={{ backgroundColor: Color.white }}>
         <Text style={[TYPOS.body2, { color: Color.neutral1, padding: 16 }]}>
