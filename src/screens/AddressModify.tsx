018f2dd928c57dc6342ed7b4b60e81420a5a940f
yuntaengtaeng <k76253218@gmail.com>
2023-09-26 22:47:11 +0900

    refactor : AddressModify 동네가 이미 존재하거나 2개인 경우 사용자에게 보여지는 Dialog 띄우는 방식을 useOverlay를 사용하도록 변경

--- src/screens/AddressModify.tsx
+++ src/screens/AddressModify.tsx
@@ -1,13 +1,13 @@
 import { StackScreenProps } from '@react-navigation/stack';
-import React, { useState } from 'react';
+import React from 'react';
 import Address from '../components/form/Address';
 import { RootStackParamList } from '../types/navigation';
 import { Location } from '../types/interface';
 import { useSetRecoilState } from 'recoil';
 import { LoadingState } from '../store/atoms';
 import axios, { AxiosError } from 'axios';
-import useModal from '../hooks/useModal';
 import Dialog from '../components/ui/Dialog';
+import useOverlay from '../hooks/overlay/useOverlay';
 
 export type AddressRegistrationScreenProps = StackScreenProps<
   RootStackParamList,
@@ -18,9 +18,30 @@ const AddressModify = ({
   navigation,
   route,
 }: AddressRegistrationScreenProps) => {
-  const { isVisible, openModal, closeModal } = useModal();
-  const [errorMessage, setErrorMessage] = useState('');
+  const overlay = useOverlay();
   const setIsLoading = useSetRecoilState(LoadingState);
+
+  const openErrorDialog = (status: 400 | 412) => {
+    overlay.open(
+      <Dialog isOpened={true}>
+        <Dialog.Title
+          title={
+            status === 400
+              ? '동네가 이미 존재해요.'
+              : '동네가 2개라 추가할 수 없어요.'
+          }
+        />
+        <Dialog.Buttons
+          buttons={[
+            {
+              label: '확인',
+              onPressHandler: overlay.close,
+            },
+          ]}
+        />
+      </Dialog>
+    );
+  };
 
   const addressSelectHandler = async ({
     location,
@@ -48,35 +69,14 @@ const AddressModify = ({
       - 동네가 이미 2개인 경우
       */
       if (errorResponse?.status === 400 || errorResponse?.status === 412) {
-        setErrorMessage(
-          errorResponse?.status === 400
-            ? '동네가 이미 존재해요.'
-            : '동네가 2개라 추가할 수 없어요.'
-        );
-
-        openModal();
+        openErrorDialog(errorResponse?.status);
       }
     } finally {
       setIsLoading(false);
     }
   };
 
-  return (
-    <>
-      <Address addressSelectHandler={addressSelectHandler} />
-      <Dialog isOpened={isVisible}>
-        <Dialog.Title title={errorMessage} />
-        <Dialog.Buttons
-          buttons={[
-            {
-              label: '확인',
-              onPressHandler: closeModal,
-            },
-          ]}
-        />
-      </Dialog>
-    </>
-  );
+  return <Address addressSelectHandler={addressSelectHandler} />;
 };
 
 export default AddressModify;
