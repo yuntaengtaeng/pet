3cbe005de56c7c79670aabfb045d6370b572691d
yuntaengtaeng <k76253218@gmail.com>
2023-09-27 21:09:13 +0900

    feat : MyPage 더미데이터를 활용하여 UI 구현

--- src/screens/bottomTab/MyPage.tsx
+++ src/screens/bottomTab/MyPage.tsx
@@ -4,28 +4,35 @@ import {
   RootStackParamList,
   TabNavigatorParamList,
 } from '../../types/navigation';
-import Container from '../../components/layout/Container';
-import { Text } from 'react-native';
-import TYPOS from '../../components/ui/typo';
 import {
   CompositeNavigationProp,
   useNavigation,
 } from '@react-navigation/native';
 import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
+import ScrollContainer from '../../components/layout/ScrollContainer';
+import Header from '../../components/myPage/Header';
+import Profile from '../../components/myPage/Profile';
+import PetInfo from '../../components/myPage/PetInfo';
+import Menu from '../../components/myPage/Menu';
 
-type HomeScreenProps = CompositeNavigationProp<
+type MyPetScreenProps = CompositeNavigationProp<
   BottomTabNavigationProp<TabNavigatorParamList, 'MyPage'>,
   StackNavigationProp<RootStackParamList>
 >;
 
-const MyPage = () => {
-  const navigation = useNavigation<HomeScreenProps>();
+const MyPet = () => {
+  const navigation = useNavigation<MyPetScreenProps>();
 
   return (
-    <Container>
-      <Text style={[TYPOS.headline1]}>MyPage</Text>
-    </Container>
+    <>
+      <Header />
+      <ScrollContainer>
+        <Profile />
+        <PetInfo />
+        <Menu />
+      </ScrollContainer>
+    </>
   );
 };
 
-export default MyPage;
+export default MyPet;
