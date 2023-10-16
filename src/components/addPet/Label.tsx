670e924dfb67a3fe6720c5fbec50c818d5a17216
yuntaengtaeng <k76253218@gmail.com>
2023-10-16 22:35:25 +0900

    add : addPet/Label 입력 요소의 타이틀 component

---
+++ src/components/addPet/Label.tsx
@@ -0,0 +1,19 @@
+import { Text } from 'react-native';
+import Color from '../../constants/color';
+import TYPOS from '../ui/typo';
+
+interface Props {
+  label: string;
+  required?: boolean;
+}
+
+const Label = ({ label, required }: Props) => {
+  return (
+    <Text style={[TYPOS.body2, { color: Color.neutral1, marginBottom: 8 }]}>
+      {label}
+      {required && <Text style={[TYPOS.body2, { color: Color.error }]}>*</Text>}
+    </Text>
+  );
+};
+
+export default Label;
