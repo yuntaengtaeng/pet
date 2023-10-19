7151a852802bedf5b0cc755d83ccda8e52d46307
yuntaengtaeng <k76253218@gmail.com>
2023-10-19 23:20:04 +0900

    feat : bottomTab/Chatting 채팅 목록 ChatList 컴포넌트를 사용하도록 변경

--- src/screens/bottomTab/Chatting.tsx
+++ src/screens/bottomTab/Chatting.tsx
@@ -1,57 +1,18 @@
-import React, { useContext, useEffect, useRef, useState } from 'react';
-import { StackNavigationProp } from '@react-navigation/stack';
-import {
-  RootStackParamList,
-  TabNavigatorParamList,
-} from '../../types/navigation';
-import { FlatList, Text, View } from 'react-native';
+import React, { useContext, useEffect, useState } from 'react';
+import { Text, View } from 'react-native';
 import TYPOS from '../../components/ui/typo';
-import { CompositeNavigationProp } from '@react-navigation/native';
-import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
-import ChatRoomItem from '../../components/ui/ChatRoomItem';
 import Color from '../../constants/color';
-import EmptyList from '../../components/chat/EmptyList';
 import { WebSocketContext } from '../../components/WebSocketContainer';
 import { useRecoilValue } from 'recoil';
 import { UserState } from '../../store/atoms';
-import axios from 'axios';
-import useOverlay from '../../hooks/overlay/useOverlay';
-import Dialog from '../../components/ui/Dialog';
-import { Swipeable } from 'react-native-gesture-handler';
-import Button from '../../components/ui/buttons/Button';
-
-type HomeScreenProps = CompositeNavigationProp<
-  BottomTabNavigationProp<TabNavigatorParamList, 'Chatting'>,
-  StackNavigationProp<RootStackParamList>
->;
-
-interface RoomData {
-  id: string;
-  title: string;
-  lastChat: string;
-  lastChatAt: string;
-  isAlarm: boolean;
-  isPinned: boolean;
-  isPetMate?: boolean;
-  image?: string;
-  region: string;
-  productImage?: string;
-}
+import { Room } from '../../types/interface';
+import ChatList from '../../components/chat/ChatList';
 
 const Chatting = () => {
-  const [rooms, setRooms] = useState<RoomData[]>([]);
+  const [rooms, setRooms] = useState<Room[]>([]);
   const socket = useContext(WebSocketContext);
-  const rowRef = useRef<Swipeable | null>(null);
-
-  const closeRow = () => {
-    if (rowRef.current) {
-      rowRef.current.close();
-      rowRef.current = null;
-    }
-  };
 
   const { accessToken } = useRecoilValue(UserState);
-  const overlay = useOverlay();
 
   useEffect(() => {
     if (!socket) return;
@@ -72,59 +33,6 @@ const Chatting = () => {
     };
   }, [socket]);
 
-  const onPinPressHandler = async (id: string) => {
-    try {
-      const {
-        data: { chatRoomList },
-      } = await axios.patch(`/chat/pinned/${id}`);
-      setRooms(chatRoomList);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const onExitPressHandler = async (id: string) => {
-    overlay.open(
-      <Dialog isOpened={true}>
-        <Dialog.Content content="채팅방을 나가면 대화 내용이 모두 삭제되며 복구할 수 없어요. 채팅방을 나갈까요?" />
-        <Dialog.Buttons
-          buttons={[
-            {
-              label: '취소',
-              onPressHandler: overlay.close,
-            },
-            {
-              label: '나가기',
-              onPressHandler: async () => {
-                try {
-                  const {
-                    data: { chatRoomList },
-                  } = await axios.patch(`/chat/leave/${id}`);
-                  setRooms(chatRoomList);
-                } catch (error) {
-                  console.log(error);
-                } finally {
-                  overlay.close();
-                }
-              },
-            },
-          ]}
-        />
-      </Dialog>
-    );
-  };
-
-  const onToggleNotificationHandler = async (id: string) => {
-    try {
-      const {
-        data: { chatRoomList },
-      } = await axios.patch(`/chat/alarm/${id}`);
-      setRooms(chatRoomList);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   return (
     <>
       <View
@@ -136,48 +44,11 @@ const Chatting = () => {
       >
         <Text style={[TYPOS.headline3, { color: Color.black }]}>채팅</Text>
       </View>
-      <FlatList
-        contentContainerStyle={{
-          backgroundColor: Color.white,
-          flex: 1,
+      <ChatList
+        rooms={rooms}
+        handleRoomsDataUpdate={(updateRooms) => {
+          setRooms(updateRooms);
         }}
-        keyExtractor={(item) => item.id}
-        data={rooms}
-        showsVerticalScrollIndicator={false}
-        renderItem={({ item }) => (
-          <ChatRoomItem
-            roomId={item.id}
-            image={item.image}
-            roomName={item.title}
-            region={item.region}
-            timeStamp={item.lastChatAt}
-            content={item.lastChat}
-            isPinned={item.isPinned}
-            isNotificationEnabled={item.isAlarm}
-            onPinPressHandler={() => {
-              onPinPressHandler(item.id);
-              closeRow();
-              rowRef.current = null;
-            }}
-            onExitPressHandler={() => {
-              onExitPressHandler(item.id);
-              closeRow();
-              rowRef.current = null;
-            }}
-            onToggleNotificationHandler={() => {
-              onToggleNotificationHandler(item.id);
-              closeRow();
-              rowRef.current = null;
-            }}
-            setSwipeable={(ref) => {
-              if (rowRef.current && ref !== rowRef.current) {
-                closeRow();
-              }
-              rowRef.current = ref;
-            }}
-          />
-        )}
-        ListEmptyComponent={<EmptyList />}
       />
     </>
   );
