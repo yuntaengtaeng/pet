c25e57a9cb7ec9ea81501a593d59862389fd323d
yuntaengtaeng <k76253218@gmail.com>
2023-10-26 20:22:28 +0900

    refactor : addPet 기존 로직을 form/Pet 컴포넌트를 사용하도록 변경

--- src/screens/myPage/AddPet.tsx
+++ src/screens/myPage/AddPet.tsx
@@ -1,77 +1,20 @@
-import ScrollContainer from '../../components/layout/ScrollContainer';
 import Header from '../../components/ui/Header';
-import PhotoSelector, {
-  ImageType,
-} from '../../components/form/Product/PhotoSelector';
-import { View, Text } from 'react-native';
-import TYPOS from '../../components/ui/typo';
-import Color from '../../constants/color';
-import InputField from '../../components/ui/inputs/InputField';
 import { StackScreenProps } from '@react-navigation/stack';
 import { RootStackParamList } from '../../types/navigation';
-import RadioButtonGroup from '../../components/ui/radio/RadioButtonGroup';
-import RadioButtonItem from '../../components/ui/radio/RadioButtonItem';
-import TextArea from '../../components/ui/inputs/TextArea';
-import TabSelector, { Tap } from '../../components/addPet/TabSelector';
-import { useReducer, useState } from 'react';
-import Button from '../../components/ui/buttons/Button';
-import Label from '../../components/addPet/Label';
-import SpeciesPicker from '../../components/addPet/SpeciesPicker';
-import BirthdaySelector from '../../components/addPet/BirthdaySelector';
-import { Date } from '../../types/interface';
+import { Tap } from '../../components/form/Pet/TabSelector';
 import * as MediaLibrary from 'expo-media-library';
 import { useSetRecoilState } from 'recoil';
 import { LoadingState } from '../../store/atoms';
 import axios from 'axios';
+import Pet, { Data } from '../../components/form/Pet';
 
 export type AddPetScreenProps = StackScreenProps<RootStackParamList, 'AddPet'>;
-
-type NeuteredStatus = '미완료' | '완료' | '모름';
-type Gender = 'M' | 'F';
-
-interface Data {
-  petImages: ImageType[];
-  name: string;
-  birthday: Date;
-  species: string;
-  weight: string;
-  gender: Gender;
-  neuteredStatus: NeuteredStatus;
-  unusualCondition: string;
-  helloMessage: string;
-}
 
 const AddPet = ({ navigation, route }: AddPetScreenProps) => {
   const { type } = route.params;
   const setIsLoading = useSetRecoilState(LoadingState);
 
-  const [tap, setTap] = useState<Tap>(0);
-  const [data, updateData] = useReducer(
-    (prev: Data, next: Partial<Data>) => {
-      return { ...prev, ...next };
-    },
-    {
-      petImages: [],
-      name: '',
-      birthday: {
-        yyyy: '',
-        m: '',
-        d: '',
-      },
-      species: '',
-      weight: '',
-      gender: 'M',
-      neuteredStatus: '미완료',
-      unusualCondition: '',
-      helloMessage: '',
-    }
-  );
-
-  const onChange = (tap: Tap) => {
-    setTap(tap);
-  };
-
-  const onSubmit = async () => {
+  const onSubmit = async (data: Data, tap: Tap) => {
     const formData = new FormData();
     formData.append('type', type);
     formData.append('name', data.name);
@@ -117,138 +60,7 @@ const AddPet = ({ navigation, route }: AddPetScreenProps) => {
   return (
     <>
       <Header title="반려동물 정보" />
-      <ScrollContainer
-        style={{ paddingHorizontal: 16, gap: 24, paddingBottom: 24 }}
-      >
-        <View>
-          <PhotoSelector
-            selectedPhotos={data.petImages}
-            updatePhotos={(photos) => {
-              updateData({ petImages: [...data.petImages, ...photos] });
-            }}
-            deletePhoto={(id) => {
-              const clone = [...data.petImages];
-              const findIndex = clone.findIndex((clone) => clone.id === id);
-              if (findIndex !== -1) {
-                clone.splice(findIndex, 1);
-              }
-              updateData({ petImages: clone });
-            }}
-          />
-        </View>
-        <View>
-          <Label label="이름" required />
-          <InputField
-            placeholder="이름을 입력하세요."
-            value={data.name}
-            onChangeHandler={(value) => {
-              updateData({ name: value });
-            }}
-          />
-        </View>
-        <View>
-          <Label label={type === 'cat' ? '묘종' : '견종'} required />
-          <TabSelector selectedTap={tap} onSelectedHandler={onChange} />
-          <View style={{ marginTop: 16 }}></View>
-          <SpeciesPicker
-            petType={type}
-            value={data.species}
-            onChangeHandler={(value) => {
-              updateData({ species: value });
-            }}
-            type={tap === 0 ? '목록에서 선택' : '직접 입력'}
-          />
-        </View>
-        <View>
-          <Label label="생년월일" required />
-          <BirthdaySelector
-            value={data.birthday}
-            onChangeHandler={(date) => {
-              updateData({ birthday: date });
-            }}
-          />
-        </View>
-        <View>
-          <Label label="성별" />
-          <RadioButtonGroup
-            containerStyle={{ flexDirection: 'row', gap: 16 }}
-            onSelected={(value) => {
-              updateData({ gender: value as Gender });
-            }}
-            selected={data.gender}
-          >
-            <RadioButtonItem value="M">
-              <Text style={[TYPOS.body1, { color: Color.neutral1 }]}>남아</Text>
-            </RadioButtonItem>
-            <RadioButtonItem value="F">
-              <Text style={[TYPOS.body1, { color: Color.neutral1 }]}>여아</Text>
-            </RadioButtonItem>
-          </RadioButtonGroup>
-        </View>
-        <View>
-          <Label label="중성화 여부" />
-          <RadioButtonGroup
-            containerStyle={{ flexDirection: 'row', gap: 16 }}
-            onSelected={(value) => {
-              updateData({ neuteredStatus: value as NeuteredStatus });
-            }}
-            selected={data.neuteredStatus}
-          >
-            <RadioButtonItem value="미완료">
-              <Text style={[TYPOS.body1, { color: Color.neutral1 }]}>
-                미완료
-              </Text>
-            </RadioButtonItem>
-            <RadioButtonItem value="완료">
-              <Text style={[TYPOS.body1, { color: Color.neutral1 }]}>완료</Text>
-            </RadioButtonItem>
-            <RadioButtonItem value="모름">
-              <Text style={[TYPOS.body1, { color: Color.neutral1 }]}>모름</Text>
-            </RadioButtonItem>
-          </RadioButtonGroup>
-        </View>
-        <View>
-          <Label label="몸무게" />
-          <View style={{ flexDirection: 'row', gap: 16, alignItems: 'center' }}>
-            <InputField
-              placeholder="몸무게를 입력하세요."
-              layoutStyle={{ width: 184 }}
-              keyboardType="decimal-pad"
-              onChangeHandler={(value) => {
-                updateData({ weight: value });
-              }}
-            />
-            <Text style={[TYPOS.body1, { color: Color.neutral1 }]}>kg</Text>
-          </View>
-        </View>
-        <View>
-          <Label label="특이사항" />
-          <TextArea
-            placeholder="특이사항을 입력하세요."
-            fieldStyle={{ minHeight: 64 }}
-            maxLength={50}
-            value={data.unusualCondition}
-            onChangeHandler={(value) => {
-              updateData({ unusualCondition: value });
-            }}
-          />
-        </View>
-        <View>
-          <Label label="인사말" />
-          <TextArea
-            placeholder="인사말을 입력하세요."
-            fieldStyle={{ minHeight: 84 }}
-            maxLength={100}
-            value={data.helloMessage}
-            onChangeHandler={(value) => {
-              updateData({ helloMessage: value });
-            }}
-          />
-        </View>
-      </ScrollContainer>
-      <View style={{ paddingHorizontal: 16 }}>
-        <Button label="저장" buttonType="primary" onPressHandler={onSubmit} />
-      </View>
+      <Pet type={type} onSubmitHandler={onSubmit} />
     </>
   );
 };
