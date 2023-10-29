7c2c805c3eef54ccb4e35fd2d3121e0532cb0ca1
yuntaengtaeng <k76253218@gmail.com>
2023-10-29 22:02:27 +0900

    add : ui/Tag Component

---
+++ src/components/ui/Tag.tsx
@@ -0,0 +1,42 @@
+import { View, Text } from 'react-native';
+import Color from '../../constants/color';
+import TYPOS from './typo';
+
+interface Props {
+  label: string;
+  theme: 'warning' | 'success';
+}
+
+const Tag = ({ label, theme }: Props) => {
+  const [color, bgColor] = (() => {
+    switch (theme) {
+      case 'success':
+        return [Color.success, Color.successBg];
+      case 'warning':
+        return [Color.warning, Color.warningBg];
+    }
+  })();
+
+  return (
+    <View
+      style={{
+        backgroundColor: bgColor,
+        alignSelf: 'flex-start',
+      }}
+    >
+      <Text
+        style={[
+          TYPOS.body3,
+          {
+            color: color,
+            padding: 4,
+          },
+        ]}
+      >
+        {label}
+      </Text>
+    </View>
+  );
+};
+
+export default Tag;
