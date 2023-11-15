eee69e5d5639684fe7395210b6b3295c9c772501
yuntaengtaeng <k76253218@gmail.com>
2023-11-15 22:01:50 +0900

    feat : VerticalProductCard 이미지가 없는 경우 Default 이미지를 사용하도록 변경

--- src/components/ui/VerticalProductCard.tsx
+++ src/components/ui/VerticalProductCard.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Image, Pressable, Text, ViewStyle } from 'react-native';
+import { Image, Pressable, Text, View, ViewStyle } from 'react-native';
 import TYPOS from './typo';
 import Color from '../../constants/color';
+import Image32 from './icons/Image32';
 
 interface Props {
   image: string;
@@ -17,18 +18,35 @@ const VerticalProductCard = (props: Props) => {
       style={[props.style, { width: 160 }]}
       onPress={props.onPressHandler}
     >
-      <Image
-        style={[
-          {
+      {!!props.image ? (
+        <Image
+          style={[
+            {
+              width: 160,
+              height: 160,
+              resizeMode: 'cover',
+              borderRadius: 8,
+              marginBottom: 8,
+            },
+          ]}
+          source={{ uri: props.image }}
+        />
+      ) : (
+        <View
+          style={{
             width: 160,
             height: 160,
-            resizeMode: 'cover',
             borderRadius: 8,
             marginBottom: 8,
-          },
-        ]}
-        source={{ uri: props.image }}
-      />
+            backgroundColor: Color.neutral5,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <Image32 color={Color.neutral4} />
+        </View>
+      )}
+
       <Text
         style={[TYPOS.body1, { color: Color.black, marginBottom: 4 }]}
         numberOfLines={1}
