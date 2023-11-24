740a614b0f7c466a6a76f9e2f088d26577473799
yuntaengtaeng <k76253218@gmail.com>
2023-11-24 20:41:22 +0900

    feat : BottomSheet ToastMessage를 띄우는 showToastMessage function을 useImperativeHandle를 이용하여 부모 컴포넌트에서도 접근 가능하도록 변경

--- src/components/ui/BottomSheet.tsx
+++ src/components/ui/BottomSheet.tsx
@@ -1,4 +1,9 @@
-import React, { useEffect, useRef } from 'react';
+import React, {
+  useEffect,
+  useRef,
+  forwardRef,
+  useImperativeHandle,
+} from 'react';
 import {
   View,
   StyleSheet,
@@ -12,6 +17,8 @@ import {
 import Color from '../../constants/color';
 import TYPOS from './typo';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import useToastState from '../../hooks/useToastState';
+import ToastMessage from './toast/ToastMessage';
 
 interface BottomSheetProps {
   isOpened: boolean;
@@ -21,106 +28,122 @@ interface BottomSheetProps {
   height?: number;
 }
 
-const BottomSheet = ({
-  isOpened,
-  title,
-  onClose,
-  children,
-  height,
-}: BottomSheetProps) => {
-  const insets = useSafeAreaInsets();
-  const screenHeight = Dimensions.get('screen').height;
-  const panY = useRef(new Animated.Value(screenHeight)).current;
-  const translateY = panY.interpolate({
-    inputRange: [-1, 0, 1],
-    outputRange: [0, 0, 1],
-  });
+export interface BottomSheetRef {
+  showToastMessage: (message: string) => void;
+}
 
-  const resetBottomSheet = Animated.timing(panY, {
-    toValue: 0,
-    duration: 300,
-    useNativeDriver: true,
-  });
+/*
+  legacy
+  
+  Modal 위에서 Toast를 띄울 수 없는 상황으로 ToastContainer의 기능을 사용하지 않고
+  직접 ToastMessage 컴포넌트를 Modal 내부에서 랜더링 시키도록 구현
+  BottomSheet를 사용하는 곳에서 ToastMessage를 띄우기 위해 showToastMessage를 useImperativeHandle 이용하여 부모 컴포넌트에서 접근이 가능하도록 구현
+*/
 
-  const closeBottomSheet = Animated.timing(panY, {
-    toValue: screenHeight,
-    duration: 300,
-    useNativeDriver: true,
-  });
-
-  const panResponders = useRef(
-    PanResponder.create({
-      onStartShouldSetPanResponder: () => true,
-      onMoveShouldSetPanResponder: () => false,
-      onPanResponderMove: (event, gestureState) => {
-        panY.setValue(gestureState.dy);
-      },
-      onPanResponderRelease: (event, gestureState) => {
-        if (
-          gestureState.dy > 0 &&
-          (gestureState.dy > 80 || gestureState.vy > 1.5)
-        ) {
-          closeModal();
-        } else {
-          resetBottomSheet.start();
-        }
-      },
-    })
-  ).current;
-
-  useEffect(() => {
-    if (isOpened) {
-      resetBottomSheet.start();
-    }
-  }, [isOpened]);
-
-  const closeModal = () => {
-    closeBottomSheet.start(() => {
-      onClose();
+const BottomSheet = forwardRef<BottomSheetRef, BottomSheetProps>(
+  ({ isOpened, title, onClose, children, height }, ref) => {
+    const [toastState, showToastMessage] = useToastState();
+    const insets = useSafeAreaInsets();
+    const screenHeight = Dimensions.get('screen').height;
+    const panY = useRef(new Animated.Value(screenHeight)).current;
+    const translateY = panY.interpolate({
+      inputRange: [-1, 0, 1],
+      outputRange: [0, 0, 1],
     });
-  };
 
-  return (
-    <Modal animationType="fade" transparent={true} visible={isOpened}>
-      <Pressable
-        onPress={() => {
-          closeModal();
-        }}
-        style={styles.overlay}
-      ></Pressable>
-      <Animated.View
-        style={[
-          styles.bottomSheetContainer,
-          {
-            height,
-            transform: [{ translateY: translateY }],
-            paddingBottom: insets.bottom,
-          },
-        ]}
-      >
-        <View {...panResponders.panHandlers}>
-          <View style={{ alignItems: 'center' }}>
-            <View
-              style={{
-                width: 44,
-                height: 4,
-                backgroundColor: '#DEDEDE',
-                marginVertical: 8,
-                borderRadius: 4,
-              }}
-            ></View>
+    const resetBottomSheet = Animated.timing(panY, {
+      toValue: 0,
+      duration: 300,
+      useNativeDriver: true,
+    });
+
+    const closeBottomSheet = Animated.timing(panY, {
+      toValue: screenHeight,
+      duration: 300,
+      useNativeDriver: true,
+    });
+
+    const panResponders = useRef(
+      PanResponder.create({
+        onStartShouldSetPanResponder: () => true,
+        onMoveShouldSetPanResponder: () => false,
+        onPanResponderMove: (event, gestureState) => {
+          panY.setValue(gestureState.dy);
+        },
+        onPanResponderRelease: (event, gestureState) => {
+          if (
+            gestureState.dy > 0 &&
+            (gestureState.dy > 80 || gestureState.vy > 1.5)
+          ) {
+            closeModal();
+          } else {
+            resetBottomSheet.start();
+          }
+        },
+      })
+    ).current;
+
+    useEffect(() => {
+      if (isOpened) {
+        resetBottomSheet.start();
+      }
+    }, [isOpened]);
+
+    const closeModal = () => {
+      closeBottomSheet.start(() => {
+        onClose();
+      });
+    };
+
+    useImperativeHandle(ref, () => ({
+      showToastMessage: (message: string) => {
+        showToastMessage(message);
+      },
+    }));
+
+    return (
+      <Modal animationType="fade" transparent={true} visible={isOpened}>
+        <Pressable
+          onPress={() => {
+            closeModal();
+          }}
+          style={styles.overlay}
+        ></Pressable>
+        <Animated.View
+          style={[
+            styles.bottomSheetContainer,
+            {
+              height,
+              transform: [{ translateY: translateY }],
+              paddingBottom: insets.bottom,
+            },
+          ]}
+        >
+          <View {...panResponders.panHandlers}>
+            <View style={{ alignItems: 'center' }}>
+              <View
+                style={{
+                  width: 44,
+                  height: 4,
+                  backgroundColor: '#DEDEDE',
+                  marginVertical: 8,
+                  borderRadius: 4,
+                }}
+              ></View>
+            </View>
+            <View style={{ paddingVertical: 24, paddingHorizontal: 16 }}>
+              <Text style={[TYPOS.headline3, { color: Color.black }]}>
+                {title}
+              </Text>
+            </View>
           </View>
-          <View style={{ paddingVertical: 24, paddingHorizontal: 16 }}>
-            <Text style={[TYPOS.headline3, { color: Color.black }]}>
-              {title}
-            </Text>
-          </View>
-        </View>
-        <View>{children}</View>
-      </Animated.View>
-    </Modal>
-  );
-};
+          <View>{children}</View>
+        </Animated.View>
+        {toastState.isVisible && <ToastMessage message={toastState.message} />}
+      </Modal>
+    );
+  }
+);
 
 export default BottomSheet;
 
