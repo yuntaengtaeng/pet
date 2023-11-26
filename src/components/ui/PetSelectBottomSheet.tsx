a8054fab38fc78d8ad5e4c50a946e96f7b18d8a5
yuntaengtaeng <k76253218@gmail.com>
2023-11-26 14:08:40 +0900

    add : PetSelectBottomSheet component

---
+++ src/components/ui/PetSelectBottomSheet.tsx
@@ -0,0 +1,84 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { View } from 'react-native';
+import { Pet } from '../../types/interface';
+import BottomSheet from './BottomSheet';
+import Button from './buttons/Button';
+import PetLabel from './PetLabel';
+
+interface Props {
+  isVisible: boolean;
+  onClose: () => void;
+  selectedPets?: Pet[];
+  successButtonHandler: (pets: Pet[]) => void;
+}
+
+const PetSelectBottomSheet = ({
+  isVisible,
+  onClose,
+  selectedPets = [],
+  successButtonHandler,
+}: Props) => {
+  const [pets, setPets] = useState<Pet[]>([]);
+  const [localSelectedPets, setLocalSelectedPets] = useState<Pet[]>([
+    ...selectedPets,
+  ]);
+
+  useEffect(() => {
+    const fetch = async () => {
+      const result = await axios.get<{ pets: Pet[] }>('/my-page/pets');
+      setPets(result.data.pets);
+    };
+
+    fetch();
+  }, []);
+
+  const selectedIds = [...localSelectedPets].map((pet) => pet.id);
+
+  return (
+    <BottomSheet isOpened={isVisible} title="함께할 반려동물" onClose={onClose}>
+      <View>
+        <View>
+          {pets.map((pet) => {
+            const isChecked = selectedIds.includes(pet.id);
+
+            return (
+              <PetLabel
+                key={pet.id}
+                pet={pet}
+                isChecked={isChecked}
+                onPressHandler={() => {
+                  if (isChecked) {
+                    const filteredPets = [...localSelectedPets].filter(
+                      (p) => pet.id !== p.id
+                    );
+                    setLocalSelectedPets(filteredPets);
+                  } else {
+                    setLocalSelectedPets([...localSelectedPets, pet]);
+                  }
+                }}
+              />
+            );
+          })}
+        </View>
+        <View
+          style={{
+            paddingTop: 24,
+            paddingBottom: 32,
+            paddingHorizontal: 16,
+          }}
+        >
+          <Button
+            label="완료"
+            onPressHandler={() => {
+              onClose();
+              successButtonHandler(localSelectedPets);
+            }}
+          />
+        </View>
+      </View>
+    </BottomSheet>
+  );
+};
+
+export default PetSelectBottomSheet;
