fc53769531b60eb08358018c91526aba14de46c8
yuntaengtaeng <k76253218@gmail.com>
2023-12-22 21:24:29 +0900

    feat : PetSelectBottomSheet Props limit 추가 및 limit 만큼 선택 시 PetLabel disabled 처리 추가
    - disabled 처리 된 PetLabel Press 시 ToastMessage로 안내 추가

--- src/components/ui/PetSelectBottomSheet.tsx
+++ src/components/ui/PetSelectBottomSheet.tsx
@@ -1,9 +1,8 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { View } from 'react-native';
 import { getRegisteredDogsList } from '../../lib/api';
 import { Pet } from '../../types/interface';
-import BottomSheet from './BottomSheet';
+import BottomSheet, { BottomSheetRef } from './BottomSheet';
 import Button from './buttons/Button';
 import PetLabel from './PetLabel';
 
@@ -12,6 +11,7 @@ interface Props {
   onClose: () => void;
   selectedPets?: Pet[];
   successButtonHandler: (pets: Pet[]) => void;
+  limit?: number;
 }
 
 const PetSelectBottomSheet = ({
@@ -19,7 +19,9 @@ const PetSelectBottomSheet = ({
   onClose,
   selectedPets = [],
   successButtonHandler,
+  limit,
 }: Props) => {
+  const bottomSheetRef = useRef<BottomSheetRef>(null);
   const [pets, setPets] = useState<Pet[]>([]);
   const [localSelectedPets, setLocalSelectedPets] = useState<Pet[]>([
     ...selectedPets,
@@ -36,19 +38,40 @@ const PetSelectBottomSheet = ({
 
   const selectedIds = [...localSelectedPets].map((pet) => pet.id);
 
+  useEffect(() => {
+    if (isVisible) {
+      setLocalSelectedPets(selectedPets);
+    }
+  }, [isVisible]);
+
   return (
-    <BottomSheet isOpened={isVisible} title="함께할 반려동물" onClose={onClose}>
+    <BottomSheet
+      isOpened={isVisible}
+      title="함께할 반려동물"
+      onClose={onClose}
+      ref={bottomSheetRef}
+    >
       <View>
         <View>
           {pets.map((pet) => {
             const isChecked = selectedIds.includes(pet.id);
+            let disabled =
+              !!limit && limit === selectedIds.length && !isChecked;
 
             return (
               <PetLabel
                 key={pet.id}
                 pet={pet}
                 isChecked={isChecked}
+                disabled={disabled}
                 onPressHandler={() => {
+                  if (disabled) {
+                    bottomSheetRef.current?.showToastMessage(
+                      '선택 가능 견수를 초과했습니다.'
+                    );
+                    return;
+                  }
+
                   if (isChecked) {
                     const filteredPets = [...localSelectedPets].filter(
                       (p) => pet.id !== p.id
