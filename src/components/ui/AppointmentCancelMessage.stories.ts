cad21f797ee15ae3a853275cd988031fbe12195c
yuntaengtaeng <k76253218@gmail.com>
2023-12-26 19:20:36 +0900

    add : AppointmentCancelMessage storybook

---
+++ src/components/ui/AppointmentCancelMessage.stories.ts
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import AppointmentCancelMessage from './AppointmentCancelMessage';
+
+const meta = {
+  title: 'ui/AppointmentCancelMessage',
+  component: AppointmentCancelMessage,
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<typeof AppointmentCancelMessage>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const AppointmentCancelMessageExample: Story = {
+  args: {
+    nickname: 'nickname',
+  },
+};
