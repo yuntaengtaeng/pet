be8cf49fa2fbd74b3f267fe103824460172c076d
yuntaengtaeng <k76253218@gmail.com>
2023-12-26 19:15:16 +0900

    feat : Header 추가 및 MyMate navigation 이동 추가
    - Tab 모집 => 산책 모집으로 변경

--- src/screens/bottomTab/PetMate.tsx
+++ src/screens/bottomTab/PetMate.tsx
@@ -4,7 +4,7 @@ import {
   RootStackParamList,
   TabNavigatorParamList,
 } from '../../types/navigation';
-import { Text, View } from 'react-native';
+import { Pressable, Text, View } from 'react-native';
 import TYPOS from '../../components/ui/typo';
 import {
   CompositeNavigationProp,
@@ -15,6 +15,7 @@ import Color from '../../constants/color';
 import Tabs from '../../components/ui/Tabs';
 import Recruitment from '../../components/petMate/Recruitment';
 import WalkingLog from '../../components/petMate/WalkingLog';
+import Person24 from '../../components/ui/icons/Person24';
 
 type PetMateScreenProps = CompositeNavigationProp<
   BottomTabNavigationProp<TabNavigatorParamList, 'PetMate'>,
@@ -32,16 +33,26 @@ const PetMate = () => {
           paddingHorizontal: 24,
           paddingVertical: 16,
           backgroundColor: Color.white,
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'space-between',
         }}
       >
         <Text style={[TYPOS.headline3, { color: Color.black }]}>펫 메이트</Text>
+        <Pressable
+          onPress={() => {
+            navigation.navigate('MyMate');
+          }}
+        >
+          <Person24 color={Color.neutral1} />
+        </Pressable>
       </View>
       <Tabs
         selectedIndex={tap}
         onSelectHandler={(index) => {
           setTap(index);
         }}
-        menu={['모집', '산책 기록']}
+        menu={['산책 모집', '산책 기록']}
       />
       {tap === 0 ? <Recruitment /> : <WalkingLog />}
     </>
